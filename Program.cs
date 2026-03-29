b28230df87391aa993fe39a30d09db5828a1fdf6
JuanFelipe0215 <juanfe13q@gmail.com>
2026-03-29 15:10:32 -0500

    feat: first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿while (true)
+{
+    Console.WriteLine(@"
+    --- Centro de Entrenamiento Técnico ---
+    1. Nivel 1: Suma y validación numérica
+    2. Nivel 2: Cálculo y condicionales
+    3. Nivel 3: Manipulación de Cadenas
+    4. Nivel 4: Operaciones Lógicas (Calculadora)
+    5. Nivel 5: Análisis Numérico (Listas de enteros)
+    6. Nivel 6: Gestión de Tareas (Listas de cadenas)
+    7. Nivel 7: Arreglos y Búsqueda
+    8. Nivel 8: Programación Orientada a Objetos (Clases)
+    9. Nivel 9: CRUD de Objetos (Listas de objetos)
+    10. Salir 
+    ");
+
+    Console.Write("Seleccione un nivel para iniciar: ");
+    var nivel =  int.Parse(Console.ReadLine());
+
+    switch (nivel)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+    }
+    
+};
+
