fda505e0ea1b9b204cf3a9aa4459812662f0dca0
JuanFelipe0215 <juanfe13q@gmail.com>
2026-03-29 16:31:16 -0500

    feat: nivel uno hecho

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿while (true)
+﻿int option;
+
+do
 {
     Console.WriteLine(@"
     --- Centro de Entrenamiento Técnico ---
@@ -15,15 +17,51 @@
     ");
 
     Console.Write("Seleccione un nivel para iniciar: ");
-    var nivel =  int.Parse(Console.ReadLine());
+    int.TryParse(Console.ReadLine(), out option);
 
-    switch (nivel)
+    switch (option)
     {
         case 1:
+            string valor1;
+            string valor2;
+            int num1;
+            int num2;
+
+            Console.Write("Ingrese primer numero: ");
+            valor1 = Console.ReadLine();
+            Console.Write("Ingrese segundo numero: ");
+            valor2 = Console.ReadLine();
+
+            if (int.TryParse(valor1, out num1) && int.TryParse(valor2, out num2))
+            {
+                var resultado = num1 + num2;
+                Console.WriteLine($"El resultado de la suma {num1} + {num2} es: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Valores incorrecto");
+            }
+
             break;
         case 2:
             break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            Console.WriteLine("Hasta luego!");
+            break;
     }
-    
-};
 
+}while(option != 10);
